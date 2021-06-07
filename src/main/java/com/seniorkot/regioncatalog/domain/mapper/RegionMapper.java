96f0060ab4e0b2d140a36ff6d0a0da5d1df6449e
seniorkot <seniorkot24@gmail.com>
2021-06-08 01:11:45 +0300

    Add new methods to RegionMapper

--- src/main/java/com/seniorkot/regioncatalog/domain/mapper/RegionMapper.java
+++ src/main/java/com/seniorkot/regioncatalog/domain/mapper/RegionMapper.java
@@ -1,10 +1,10 @@
 package com.seniorkot.regioncatalog.domain.mapper;
 
 import com.seniorkot.regioncatalog.domain.model.Region;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Region Mapper interface (MyBatis).
@@ -14,7 +14,74 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface RegionMapper {
 
-    @Select("SELECT * FROM regions WHERE id = #{id}")
+    /**
+     * Fetches all regions from DB.
+     *
+     * @return {@link List} of {@link Region} entities
+     */
     @ResultMap("RegionResultMap")
-    Region getRegion(@Param("id") Long id);
+    @Select("SELECT * FROM regions")
+    List<Region> getAll();
+
+    /**
+     * Fetches a region by ID.
+     *
+     * @param id Region ID
+     * @return {@link Optional} of {@link Region} entity
+     */
+    @ResultMap("RegionResultMap")
+    @Select("SELECT * FROM regions WHERE id = #{id}")
+    Optional<Region> getById(@Param("id") Long id);
+
+    /**
+     * Fetches a region by name and short name.
+     *
+     * @param name Region name
+     * @param shortName Region short name
+     * @return {@link Optional} of {@link Region} entity
+     */
+    @ResultMap("RegionResultMap")
+    @Select("SELECT * FROM regions WHERE name = #{name} AND short_name = #{short_name}")
+    Optional<Region> getByNames(@Param("name") String name,
+                                @Param("short_name") String shortName);
+
+    /**
+     * Inserts new region to DB.
+     *
+     * @param region {@link Region} entity
+     */
+    @Insert("INSERT INTO regions (name, short_name) VALUES (#{name}, #{shortName})")
+    void insert(Region region);
+
+    /**
+     * Updates existing region by ID.
+     *
+     * @param region {@link Region} entity
+     */
+    @Update("UPDATE regions SET name = #{name}, short_name = #{shortName} WHERE id = #{id}")
+    void update(Region region);
+
+    /**
+     * Deletes a region.
+     *
+     * @param region {@link Region} entity
+     */
+    @Delete("DELETE FROM regions WHERE id = #{id}")
+    void delete(Region region);
+
+    /**
+     * Deletes a region by ID.
+     *
+     * @param id {@link Region} ID
+     */
+    @Delete("DELETE FROM regions WHERE id = #{id}")
+    void deleteById(@Param("id") Long id);
+
+    /**
+     * Returns a number of {@link Region} entities.
+     *
+     * @return Total number of entities
+     */
+    @Select("SELECT COUNT(*) FROM regions")
+    int count();
 }
--- src/test/java/com/seniorkot/regioncatalog/RegionMapperIntegrationTest.java
+++ src/test/java/com/seniorkot/regioncatalog/RegionMapperIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
- * Integration test for {@link RegionMapper}.
+ * Integration test for the {@link RegionMapper}.
  *
  * @author seniorkot
  */
@@ -22,13 +22,58 @@ public class RegionMapperIntegrationTest {
     @Autowired
     private RegionMapper regionMapper;
 
+    /**
+     * Tests the region mapper for correct SELECT statements execution.
+     */
     @Test
-    public void simpleTest() {
-        Region region = regionMapper.getRegion(1L);
+    public void idSelectTest() {
+        int count = regionMapper.count();
+        assertThat(count).isEqualTo(4);
+
+        Region region = regionMapper.getById(1L).orElse(null);
 
         assertThat(region).isNotNull();
         assertThat(region.getId()).isEqualTo(1L);
         assertThat(region.getName()).isEqualTo("г. Санкт-Петербург");
         assertThat(region.getShortName()).isEqualTo("СПб");
+
+        region = regionMapper.getByNames("г. Москва", "Мск").orElse(null);
+
+        assertThat(region).isNotNull();
+        assertThat(region.getId()).isEqualTo(2L);
+        assertThat(region.getName()).isEqualTo("г. Москва");
+        assertThat(region.getShortName()).isEqualTo("Мск");
+    }
+
+    /**
+     * Tests the region mapper for correct INSERT, UPDATE and
+     * DELETE statements execution.
+     */
+    @Test
+    public void insertUpdateDeleteTest() {
+        Region region = new Region();
+        region.setName("Test full name");
+        region.setShortName("Test short name");
+        regionMapper.insert(region);
+
+        Region fetched = regionMapper.getById(5L).orElse(null);
+
+        assertThat(fetched).isNotNull();
+        assertThat(fetched.getId()).isEqualTo(5L);
+        assertThat(fetched.getName()).isEqualTo("Test full name");
+        assertThat(fetched.getShortName()).isEqualTo("Test short name");
+
+        fetched.setName("Test full name UPDATED");
+        regionMapper.update(fetched);
+
+        fetched = regionMapper.getById(5L).orElse(null);
+        assertThat(fetched).isNotNull();
+        assertThat(fetched.getId()).isEqualTo(5L);
+        assertThat(fetched.getName()).isEqualTo("Test full name UPDATED");
+        assertThat(fetched.getShortName()).isEqualTo("Test short name");
+
+        regionMapper.delete(fetched);
+        fetched = regionMapper.getById(5L).orElse(null);
+        assertThat(fetched).isNull();
     }
 }
